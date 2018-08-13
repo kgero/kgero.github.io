7964df649609b368de0953faecedf3f2acd7fd16
kgero <katystreet@gmail.com>
2018-08-12 20:31:17 -0400

    big update

---
+++ bluepill/bluepilldissolving.md
@@ -0,0 +1,186 @@
+**blue pill dissolving**
+
+*a dat-zine by katy gero  
+written during code societies  
+at the school for poetic computation*
+
+&nbsp;
+
+&nbsp;
+
+
+___ ____ ____ is swallowed when you want to stand still,  
+to stare at the clouded sky and day dream and murmur.
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the sky, vast but clouded and it will not give you   
+answers or drag you down it asks only that you look up and see.
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the sky as the storm comes in and over  
+and you know that you want release but still you are ______.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the water, dirty, still, and dangerous, and it is blue   
+but also clear, clear as the sky which is not blue so much as spacious.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is a morpho butterfly that alters its blue as you shift left  
+and then right and in the end is about the _______ structure beneath. 
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is a butterfly that may have flown miles to get to this poem.  
+What would you fly _____ to get to?
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the blue whale, singing songs that we ______  
+understand, the long melody of the internet only ____ becoming.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is a bluebell, that droops its head as time goes on, that  
+starts its life perky but slowly ________ to everything and ____.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is a pair of old denim jeans, worn and ____, a testament  
+to all you have been _______ and all you are prepared to _____ for. 
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ tastes like blueberries, the wild ones which are small  
+and tart and they are ______, delicate things, they will get _______  
+or pummeled and everything that is worth learning about _____  
+a little protection.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the Cookie Monster, with his blue ____, who _______  
+that there are some ______ we just ____.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the base of the flame, where the soot has ______ away  
+and all that is left is the ____, which cannot glow but must _______.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the moon that comes ____, that attains blueness not  
+from your ____ but from your ____ did you know you had that power?
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is Truvada, which both ______ and ________ HIV.
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is a bluejay that ______ an alarm when _______ is near,  
+that _____ but also ________, a patter of call and release. 
+
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+___ ____ ____ is the bluegum the ____ I mistook as my ___ home  
+until I found it in __________ and _______ and gain is ____. 
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
--- main.js
+++ main.js
@@ -18,7 +18,46 @@ var paintings = [
     'src': 'img/nude.jpg',
     'colsize': 'is-2',
     'text': "This mural is painted in Senior House at MIT. The photo was taken as part of a mural documentation and preservation project. You can find more murals from the dormitory <a href='https://www.flickr.com/photos/151658333@N05/sets/72157685050910985' target='_blank'>here</a>. It was selected to be part of an exhibit about Senior House. There is a video of the exhibit <a href='https://youtu.be/D5GK7L9e5QI' target='_blank'>here</a> and a write up <a href='https://thetech.com/2018/04/19/senior-house-mural-exhibit' target='_blank'>here</a>"
-  }]
+  },
+  {
+    'src': 'img/aspect.jpg',
+    'colsize': 'is-3',
+    'text': 'Set of three paintings for a book cover. More information forthcoming!'
+  },];
+
+var books = [
+  {
+    'src': 'img/mit-img.png',
+    'colsize': 'is-2',
+    'text': "<p><u>Poems for MIT Students</u> is a chapbook of 24 poems by MIT students, for MIT students.</p>\
+    <p><br />This book is the result of a poetry competition I ran in 2013, soliciting poems from the community written about the MIT experience, telling people they could use all the internal names and references (Lobby 10, Course 4, Building 36--MIT is all about numbers) that only their peers would recognize. It was an exercise in processing our experience internally, not for others but for ourselves, though of course it also allows others a look inside.</p>\
+    <p><br />Three winners were selected and given cash prizes; 24 poems were selected for the book and 1000 copies of the book were printed for the 2013 graduation festivities. Many people helped make it all happen: Erica Funkhouser, Mary Fuller, Alexandriya Edmonds, Luke Plummer, Paulina Mustafa, Sebastian Denault, Ned Burnell, Angelique Mehmzow. Many campus groups and institute departments helped fund this project: the Literature, History, and Writing and Humanistic Studies Departments, the Art Scholars, the Office of Student Citizenship, and the Undergraduate Association. </p>\
+    <p><br />You can download a pdf version of the book <a href='files/PoemsForMITStudents.pdf'>here</a>."
+  },
+  {
+    'src': 'img/bluepill.jpg',
+    'colsize': 'is-3',
+    'text': '<u>blue pill dissolving</u> is a poem-zine written for and during Code Societies while at the School for Poetic Computation. 50 copies were made and distributed.'
+  }];
+
+var arttech = [
+  {
+    'src': 'img/slowtwitter.gif',
+    'colsize': 'is-3',
+    'text': '<u>Slow Twitter</u> is a prototype alternate Twitter interface that forces you to read twitter calmly and with intention. I am currently working a Chrome Extension version. It was originally developed during Code Societies at the School for Poetic Computation.'
+  },
+  {
+    'src': 'img/poetryengine.png',
+    'colsize': 'is-2',
+    'text': "<p><a href='poetry-engine.com' target='_blank'>Poetry Engine</a> comes from a very human desire: to share something I find meaningful. Poems are hard to love, but when one makes it into your soul it can be the most powerful thing. How do we find poems to love?</p>\
+    <p><br />This is my idea: you might have one or two poems you really love. You found them at some point in your life and they just clicked. If I could give you one more poem you love, you can sit with it and take it into your heart and love it. One more poem! Amazing. And then, maybe, one more poem you love. Slowly you build a larger library of poems. Eventually it becomes easier to love new ones.\
+    <p><br />The Poetry Recommendation Engine takes one poem and tries to give you one other poem that can touch you. You can take it from there."
+  },
+  {
+    'src': 'img/code-societies.jpg',
+    'colsize': 'is-2',
+    'text': "<p><a href='https://github.com/SFPC/codesocieties' target='_blank'>Code Societies</a> was a three week long summer session at <a href='sfpc.io' target='_blank'>the School for Poetic Computation</a> I attended in July 2018. You can find the work I did <a href='dat-portfolio/index.html'>here</a>."
+  }];
 
 var meche = [
   {
@@ -58,17 +97,17 @@ var papers_cs = [
   }];
 
 var papers_other = [
-  {
-    'title': "Biometeorological indices explain outside dwelling patterns based on Wi-Fi data",
-    'author': "Christoph Reinhart, Jay Dhariwal, and <span class='myname'>Katy Gero</span>",
-    'journal': "Building and Environment",
-    'source': "Building and Environment",
-    'volume': "126",
-    'pages': "422--430",
-    'year': "2017",
-    'publisher': "Elsevier",
-    'pdf': 'https://www.sciencedirect.com/science/article/pii/S0360132317304845'
-  },
+  // {
+  //   'title': "Biometeorological indices explain outside dwelling patterns based on Wi-Fi data",
+  //   'author': "Christoph Reinhart, Jay Dhariwal, and <span class='myname'>Katy Gero</span>",
+  //   'journal': "Building and Environment",
+  //   'source': "Building and Environment",
+  //   'volume': "126",
+  //   'pages': "422--430",
+  //   'year': "2017",
+  //   'publisher': "Elsevier",
+  //   'pdf': 'https://www.sciencedirect.com/science/article/pii/S0360132317304845'
+  // },
   {
     'title': "Design and analysis of a robust, low-cost, highly articulated manipulator enabled by jamming of granular media",
     'author': "Nadia Cheng, Maxim Lobovsky, Steven Keating, Adam Setapen, <span class='myname'>Katy Gero</span>, and Anette Hosoi, and Karl Iagnemma",
@@ -119,6 +158,50 @@ $(document).ready( function() {
       tag.append("| <a target='_blank' href='" + val['slides'] + "'>slides</a> ")
     }
     $('.papers_other').append(tag);
+  });
+
+  // add writing images
+  $.each(books, function(i, val) {
+    // make thumbnails
+    var imgimg = $("<img>").attr('src', val['src']);
+    var imgfig = $("<figure class='image imageclick'>").attr('id', 'books' + i);
+    var imgdiv = $("<div class='column'>");
+    imgfig.append(imgimg);
+    imgdiv.addClass(val['colsize']);
+    imgdiv.append(imgfig);
+    $('.books').append(imgdiv);
+
+    // make modal overlays
+    var modal = $('<div>').addClass('modal').addClass('books' + i);
+    var bckgrnd = $('<div>').addClass('modal-background');
+    var newfig = imgfig.clone().removeClass('imageclick').addClass('imagemodal');
+    var text = $('<p>').addClass('textmodal').append(val['text']);
+    var content = $('<div>').addClass('modal-content').append(newfig).append(text);
+    var button = $('<button>').addClass('modal-close').addClass('is-large');
+    modal.append(bckgrnd).append(content).append(button);
+    $('body').append(modal);
+  });
+
+  // add art and tech images
+  $.each(arttech, function(i, val) {
+    // make thumbnails
+    var imgimg = $("<img>").attr('src', val['src']);
+    var imgfig = $("<figure class='image imageclick'>").attr('id', 'arttech' + i);
+    var imgdiv = $("<div class='column'>");
+    imgfig.append(imgimg);
+    imgdiv.addClass(val['colsize']);
+    imgdiv.append(imgfig);
+    $('.arttech').append(imgdiv);
+
+    // make modal overlays
+    var modal = $('<div>').addClass('modal').addClass('arttech' + i);
+    var bckgrnd = $('<div>').addClass('modal-background');
+    var newfig = imgfig.clone().removeClass('imageclick').addClass('imagemodal');
+    var text = $('<p>').addClass('textmodal').append(val['text']);
+    var content = $('<div>').addClass('modal-content').append(newfig).append(text);
+    var button = $('<button>').addClass('modal-close').addClass('is-large');
+    modal.append(bckgrnd).append(content).append(button);
+    $('body').append(modal);
   });
 
   // add painting images
